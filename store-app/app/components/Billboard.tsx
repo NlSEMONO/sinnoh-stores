b47f058b45cd8afdb88024c1a801a87d7fec4f3b
NlSEMONO <86743730+NlSEMONO@users.noreply.github.com>
2023-10-11 17:07:00 -0400

    Update Billboard.tsx

--- store-app/app/components/Billboard.tsx
+++ store-app/app/components/Billboard.tsx
@@ -7,18 +7,21 @@ const CONTENT = 'Power up the speed or special attack of your Pokemon with brand
 const Billboard = () => {
   return (
     <div className="flex mx-6 lg:mx-10 pb-8 justify-center">
-        <div className='flex flex-col justify-start md:justify-start my-4 text-center'>
+        <div className='flex flex-col justify-start md:justify-start my-4 text-center md:mx-8'>
             <h2 className='text-xl'> {TITLE} </h2>
             <p className='max-w-sm'> {CONTENT} </p>
         </div>
-        <Image
+        <img src="https://archives.bulbagarden.net/media/upload/6/6a/Dream_Choice_Scarf_Sprite.png"
+          className='w-100 h-100 md:w-150 md:h-150 md:mx-8 xl:w-250 xl:h-250'
+        ></img>
+        /* <Image
             src="https://archives.bulbagarden.net/media/upload/6/6a/Dream_Choice_Scarf_Sprite.png"
             alt="Choice Scarf"
             sizes='(max-width: 640px) 100vw, (max-width: 1200px) 100vw, 100vw'
             width={211}
             height={205}
             className='m-4'
-        ></Image>
+        ></Image> */
     </div>
   )
 }
